1271fd6e3332cc4c97938837dbd09ba0ae74b90b
goncosilva <gonco4@gmail.com>
2023-06-21 02:14:33 +0100

    For

--- sketch.js
+++ sketch.js
@@ -1,17 +1,26 @@
-let laranjas;
-
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	background(0)
+    createCanvas(windowWidth, windowHeight);
+  }
+  
+  function draw() {
+    background(255, 255, 255);
 
-}
-
-function draw() {
-
-	stroke(230);
-	strokeWeight(2);
-	noFill();
-	circle(width / 2, height / 2, height / 4);
+    fill (0,191,255)
+    circle(200,200, 200);
 
 
-}
+    fill(0,255,0)
+    circle(900, 200, 200);
+
+
+    fill(255,20,147)
+    circle(200, 500, 200);
+
+    noFill()
+    stroke (2)
+    strokeWeight(3)
+    circle(900, 500, 200);
+    
+  }
+
+  
